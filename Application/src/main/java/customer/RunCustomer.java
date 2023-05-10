8a07bc2e7bbac464a5038898f59292a194334e6e
Michael Barry <barrymichaelr@outlook.com>
2023-05-10 16:10:30 -0700

    Load javaFX to customer

--- Application/src/main/java/customer/RunCustomer.java
+++ Application/src/main/java/customer/RunCustomer.java
@@ -11,7 +11,7 @@ public class RunCustomer extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(RunCustomer.class.getResource("customer.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 520, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setTitle("AeroPlastics Secure Login");
         stage.setScene(scene);
         stage.show();
--- Application/src/main/java/module-info.java
+++ Application/src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module login.aeroplastics {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+    //initiate javafx.fxml;
     opens login to javafx.fxml;
+    opens customer to javafx.fxml;
+
+    //export sheet
     exports login;
+    exports customer;
 }
