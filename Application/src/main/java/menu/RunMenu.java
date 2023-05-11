8ef3feaeab6fba8375d27dd23ca5f4fe4673e54f
Michael Barry <barrymichaelr@outlook.com>
2023-05-10 19:53:15 -0700

    Added order GUI

---
+++ Application/src/main/java/menu/RunMenu.java
@@ -0,0 +1,23 @@
+package menu;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RunMenu extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(RunMenu.class.getResource("menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setTitle("AeroPlastics - Menu");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
