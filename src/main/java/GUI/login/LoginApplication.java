3c06b994a96c3e51c2914f88e9d3ada5c4bcf6b3
Michael Barry <barrymichaelr@outlook.com>
2023-05-23 16:59:44 -0700

    Added login functionality

--- src/main/java/GUI/login/LoginApplication.java
+++ src/main/java/GUI/login/LoginApplication.java
@@ -1,7 +1,10 @@
 package GUI.login;
 
+import GUI.ViewModel;
+import GUI.menu.MenuController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -12,12 +15,21 @@ import java.io.IOException;
  */
 public class LoginApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 520, 400);
-        stage.setTitle("AeroPlastics Secure Login");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage loginStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+        Parent root = loader.load();
+        LoginController controller = loader.getController();
+        ViewModel viewModel = new ViewModel();
+        controller.setViewModel(viewModel);
+
+        Scene scene = new Scene(root);
+        loginStage.setScene(scene);
+        loginStage.setTitle("AeroPlastics - Login");
+        loginStage.show();
+
+
+
+        viewModel.setCurrentStage(loginStage);
     }
 
     /**
--- src/main/java/GUI/login/LoginController.java
+++ src/main/java/GUI/login/LoginController.java
@@ -1,8 +1,78 @@
 package GUI.login;
 
+import GUI.JavaConnector;
+import GUI.ViewModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * The type Login controller.
  */
 public class LoginController {
 
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordTextField;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button cancelButton;
+    private JavaConnector javaConnector;
+    private ViewModel viewModel;
+    public void setViewModel(ViewModel viewModel){
+        this.viewModel = viewModel;
+    }
+
+    public LoginController() {
+        javaConnector = new JavaConnector();
+    }
+
+    @FXML
+    private void handleLoginButtonAction(ActionEvent event) throws IOException {
+        String username = usernameTextField.getText();
+        String password = passwordTextField.getText();
+
+        // Check if the username and password are valid
+        if (validateCredentials(username, password)) {
+            viewModel.showMenuWindow();
+        } else {
+            showLoginFailedAlert();
+        }
+    }
+
+    private boolean validateCredentials(String username, String password) {
+        String query = "SELECT * FROM employee WHERE username = ? AND password = ?";
+        try (Connection connection = javaConnector.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, username);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
+
+            return resultSet.next();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private void showLoginFailedAlert() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Login Failed");
+        alert.setHeaderText(null);
+        alert.setContentText("Invalid username or password.");
+        alert.showAndWait();
+    }
 }
