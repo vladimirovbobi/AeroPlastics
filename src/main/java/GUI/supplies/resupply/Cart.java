3b63d1c747360c264aa399196837c6647e51bd40
Bobi Vladimirov <bobby151511@gmail.com>
2023-05-29 12:47:58 -0700

    Created a Singleton Cart class with methods creteCart, addtoCart,addToAmount

--- src/main/java/GUI/supplies/resupply/Cart.java
+++ src/main/java/GUI/supplies/resupply/Cart.java
@@ -2,52 +2,89 @@ package GUI.supplies.resupply;
 
 import GUI.Date;
 import GUI.JavaConnector;
+import GUI.supplies.Material;
+import GUI.supplies.Vendor;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class Cart {
+    private static Cart cart;
     private double amount;
     private String orderDate,arrivalDate;
     private int cartID;
-    public Cart(){
-        cartID = getLastCartID() + 1;
+    private Cart(){
+        cartID = 1;
         amount = 0.0;
         orderDate = Date.todaysDate();
-        arrivalDate = Date.changeTodaysDateByDays(7);
+        arrivalDate = Date.changeTodaysDateByDays(5);
     }
-
-
-    public static void print() {
-        Cart c = new Cart();
-        System.out.println(c.orderDate);
-        System.out.println(c.arrivalDate);
+    public static Cart getInstance(){
+        if(cart == null) {
+            return new Cart();
+        }
+        return cart;
     }
+    public void resetCart(){
+        cartID = 1;
+        amount = 0.0;
+        orderDate = Date.todaysDate();
+        arrivalDate = Date.changeTodaysDateByDays(5);
+    }
+//this method takes Material or getMaterial from the ProductToMaterialAdapter
 
-    public int getLastCartID() {
-
-        int max = 0;
+    public void deleteCart(){
         try {
             JavaConnector javaConnector = new JavaConnector();
             Connection con = javaConnector.getConnection();
-            String query = "Select cartID from cart";
+            String query = "Delete from Cart";
             PreparedStatement statement = con.prepareStatement(query);
-            ResultSet result = statement.executeQuery();
-
-
-            while (result.next()) {
-                if (result.getInt("cartID") > max) {
-                    max = result.getInt("cartID");
-                }
-            }
+            statement.executeUpdate();
+            cart.resetCart();
 
         } catch (NumberFormatException numF1) {
 
         } catch (Exception e1) {
             e1.printStackTrace();
         }
+    }
+    public Cart createCart() throws SQLException {
 
-        return max;
+        JavaConnector javaConnector = new JavaConnector();
+        Connection con = javaConnector.getConnection();
+
+        String query = "Insert Into Cart (cartID, orderPlaced, arrivalDate)" +
+                "values (1," + ", '" + Date.todaysDate() + "', '" + Date.changeTodaysDateByDays(5) + "',";
+        PreparedStatement statement = con.prepareStatement(query);
+        statement.executeUpdate();
+        return getInstance();
+
+
+    }
+    public void addToCart(int vendorID, String materialName,int quantity){
+        try {
+            materialName.toUpperCase();
+            JavaConnector javaConnector = new JavaConnector();
+            Connection con = javaConnector.getConnection();
+
+            addToAmount(Vendor.getPriceByVendorID(vendorID,materialName)*quantity);
+
+            String query = "Insert Into Cart (amount, productID,quantity)" +
+                        "values ("+ cart.amount + Material.getIDForMaterialName(materialName) + "," +quantity+ ");";
+            PreparedStatement statement = con.prepareStatement(query);
+            statement.executeUpdate();
+
+
+        }catch(NumberFormatException numF1) {
+
+        }catch(Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+    public void addToAmount(double money){
+        this.amount += money;
+    }
+
+    public double getAmount() {
+        return amount;
     }
 }
