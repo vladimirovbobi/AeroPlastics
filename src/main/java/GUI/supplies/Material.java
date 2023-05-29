a9eb0612e87fcb4bb6a4276ae4fa6a954f6e81b1
Bobi Vladimirov <bobby151511@gmail.com>
2023-05-29 12:49:27 -0700

    added another constructor and getIDForMaterialName -> fetches id from the data base

--- src/main/java/GUI/supplies/Material.java
+++ src/main/java/GUI/supplies/Material.java
@@ -1,5 +1,12 @@
 package GUI.supplies;
 
+import GUI.Date;
+import GUI.JavaConnector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Material {
     int inventoryLevel;
     int materialID;
@@ -8,12 +15,39 @@ public class Material {
     int plasticDensity;
 
     public Material(int inventoryLevel, int materialID, String materialName, int moldTemperature, int plasticDensity){
-        this.inventoryLevel = inventoryLevel;
         this.materialID = materialID;
         this.materialName = materialName;
         this.moldTemperature = moldTemperature;
         this.plasticDensity = plasticDensity;
         this.inventoryLevel = inventoryLevel;
+    }
+    public Material (int materialID, String materialName, int inventoryLevel){
+        this.materialName = materialName;
+        this.materialID = materialID;
+        this.inventoryLevel = inventoryLevel;
+
+    }
+    public static int getIDForMaterialName(String name){
+           int i = 0 ;
+            try {
+
+                JavaConnector javaConnector = new JavaConnector();
+                Connection con = javaConnector.getConnection();
+                String query = "Select materialID from materials where materialName = '" + name + "');";
+                PreparedStatement statement = con.prepareStatement(query);
+                ResultSet resultSet= statement.executeQuery(query);
+                if(resultSet.next()){
+                    return resultSet.getInt("materialID");
+                }
+                i  = resultSet.getInt("materialID");
+
+            }catch(NumberFormatException numF1) {
+
+            }catch(Exception e1) {
+                e1.printStackTrace();
+            }
+            return i;
+
     }
 
     public int getMaterialID() {
