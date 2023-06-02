2bcbf3dfcac6a6eaf1309a4b02ff6e49df0bf42c
Bobi Vladimirov <bobby151511@gmail.com>
2023-06-01 20:14:35 -0700

    Implemented the sequential cart ID (getLastCart), removed a bug in Cart (no longer adds item if the vendor does not have it),

--- src/main/java/GUI/supplies/resupply/Cart.java
+++ src/main/java/GUI/supplies/resupply/Cart.java
@@ -6,6 +6,8 @@ import GUI.supplies.Material;
 import GUI.supplies.Vendor;
 import javafx.scene.shape.DrawMode;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -16,7 +18,7 @@ public class Cart {
     private int cartID;
     public int numberOfItems =1 ;
     private Cart(){
-        cartID = 1;
+        cartID = getLastCartId() + 1;
         this.amount = 0.0;
         orderDate = Date.todaysDate();
         arrivalDate = Date.changeTodaysDateByDays(5);
@@ -34,7 +36,30 @@ public class Cart {
         arrivalDate = Date.changeTodaysDateByDays(5);
     }
 //this method takes Material or getMaterial from the ProductToMaterialAdapter
+    public int getLastCartId(){
+        int max =0;
+        try {
+            JavaConnector javaConnector = new JavaConnector();
+            Connection con = javaConnector.getConnection();
+            String query = "Select supplyOrderID from supplyorder";
+            PreparedStatement statement = con.prepareStatement(query);
+            ResultSet result = statement.executeQuery();
 
+
+            while (result.next()) {
+                if(result.getInt("supplyOrderID")> max){
+                    max = result.getInt("supplyOrderID");
+                }
+            }
+
+        } catch (NumberFormatException numF1) {
+
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+
+        return max;
+    }
     public void deleteCart(){
         try {
             JavaConnector javaConnector = new JavaConnector();
@@ -58,12 +83,14 @@ public class Cart {
             Connection con = javaConnector.getConnection();
 
             addToAmount(Vendor.getPriceByVendorID(vendorID,materialName)*quantity);
-            String query = "Insert Into Cart (cartID,price, orderDate, arrivalDate,productID,productQuantity,vendorID,rawMaterial)" +
-                    "values (1" +","+Vendor.getPriceByVendorID(vendorID,materialName)+ ", '" + Date.todaysDate() + "', '" + Date.changeTodaysDateByDays(5) + "',"
-            +Material.getIDForMaterialName(materialName)+ ","+quantity+","+vendorID+",'"+materialName.toUpperCase()+"');";
-            PreparedStatement statement = con.prepareStatement(query);
-            statement.executeUpdate();
-
+            double price = Vendor.getPriceByVendorID(vendorID,materialName);
+            if(price > 0) {
+                String query = "Insert Into Cart (cartID,price, orderDate, arrivalDate,productID,productQuantity,vendorID,rawMaterial)" +
+                        "values ("+Cart.getInstance().cartID + "," + Vendor.getPriceByVendorID(vendorID, materialName) + ", '" + Date.todaysDate() + "', '" + Date.changeTodaysDateByDays(5) + "',"
+                        + Material.getIDForMaterialName(materialName) + "," + quantity + "," + vendorID + ",'" + materialName.toUpperCase() + "');";
+                PreparedStatement statement = con.prepareStatement(query);
+                statement.executeUpdate();
+            }
 
 
         }catch(NumberFormatException numF1) {
@@ -101,9 +128,14 @@ public class Cart {
         for(int i= 0; i< price.size(); i++) {
             total += price.get(i)*quantity.get(i);
         }
-        total = Math.round(total* 100)/100;
+        double number = total;
+        BigDecimal bd = new BigDecimal(Double.toString(number));
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        total = bd.doubleValue();
         Cart.setAmount(total);
     }
 
-
+    public int getCartID() {
+        return cartID;
+    }
 }
