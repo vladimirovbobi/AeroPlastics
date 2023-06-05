ac2619ac97bf72a135f30d3adf8ef52777b95c1a
Bobi Vladimirov <bobby151511@gmail.com>
2023-06-05 09:17:41 -0700

    productIsInStock - checks if we have enough product to fulfill the order, if we have enough it reduceProductsBy the needed amount and returns true; getMaterialUsedForProduct - retrieves the name of the material used to produce a certain product

--- src/main/java/GUI/products/Product.java
+++ src/main/java/GUI/products/Product.java
@@ -1,5 +1,12 @@
 package GUI.products;
 
+import GUI.JavaConnector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Types;
+
 public class Product{
 
     private int inventoryLevel;
@@ -38,5 +45,78 @@ public class Product{
 
     public void setInventoryLevel(int inventoryLevel) {
         this.inventoryLevel = inventoryLevel;
+    }
+
+    public static boolean productIsInStockAndShipped(int productID, int quantity){
+        try {
+            int productInStock =0;
+            String material ;
+            JavaConnector javaConnector = new JavaConnector();
+            Connection con = javaConnector.getConnection();
+            String query = "SELECT * FROM products WHERE productID = ?";
+            PreparedStatement statement = con.prepareStatement(query);
+            statement.setInt(1, productID);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                productInStock =   resultSet.getInt("inventoryLevel");
+            }
+            //produce 1
+            if (productInStock >= quantity){
+                reduceProductBy(productID, quantity);
+                return true;
+            }else{
+                return false;
+            }
+        }catch(Exception e1) {
+            e1.printStackTrace();
+        }
+        return false;
+    }
+    public static void reduceProductBy(int product, int numberOfUnitsBought){
+        int quantity = 0 ;
+        try {
+
+            JavaConnector javaConnector = new JavaConnector();
+            Connection con = javaConnector.getConnection();
+            String query = "SELECT inventoryLevel FROM products WHERE productID =? ";
+            PreparedStatement statement = con.prepareStatement(query);
+            statement.setInt(1,product);
+            ResultSet resultSet = statement.executeQuery();
+            if(resultSet.next()){
+                quantity = resultSet.getInt("inventoryLevel");
+            }
+            //Reduce the quantity because a product is made
+            quantity -= numberOfUnitsBought;
+            query = "UPDATE products SET inventoryLevel = ? WHERE productID = ?;";
+            statement = con.prepareStatement(query);
+            statement.setInt(2, product);
+
+            statement.setInt(1, quantity);
+
+            statement.executeUpdate();
+
+        }catch(Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+    public static String getMaterialUsedForProduct(int productID){
+        String material="" ;
+        try {
+            JavaConnector javaConnector = new JavaConnector();
+            Connection con = javaConnector.getConnection();
+            String query = "SELECT * FROM products WHERE productID = ?";
+            PreparedStatement statement = con.prepareStatement(query);
+            statement.setInt(1, productID);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                material = resultSet.getString("rawMaterial");
+            }
+            return material;
+        }catch(Exception e1) {
+            e1.printStackTrace();
+        }
+        return material;
     }
 }
